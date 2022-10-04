a953c4993bd2c153598f15ce728d2ffd8df890da
lontel <lonte.leon@gmail.com>
2022-10-04 11:34:05 +0200

    adding user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+require('dotenv').config()
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    firstname: {
+        type: String,
+        maxLength: 100,
+        required: true,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        maxLength: 100,
+        required: true,
+        trim: true
+    },
+    cart: {
+        type: Array,
+        default: []
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+    varified: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = { User }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,11 @@ const express = require('express')
 const authController = require('../controllers/auth.controller')
 const router = express.Router()
 
-router.get('/hello', authController.hello)
+router.post('/register', authController.register)
+router.post('/signin', authController.signin)
+router.post('/isauth', authController.isauth)
+
+
 
 
 module.exports = router
